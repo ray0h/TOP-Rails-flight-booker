c62278dcb865442578dbdc1d1219041332af4bb9
Raymond Oh <raymond.oh@gmail.com>
2020-10-10 15:28:04 -0400

    modify filtered flight results into their own selection form

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,14 @@ codes.each do |code|
 end
 
 Flight.create(dep_date: Date.new(2020, 10, 20), dep_time: Time.new(2020, 10, 20, 9, 0, 0), origination_id: 1, destination_id: 2, flight_time: '4h19m')
+Flight.create(dep_date: Date.new(2020, 10, 20), dep_time: Time.new(2020, 10, 20, 11, 15, 0), origination_id: 1, destination_id: 2, flight_time: '4h19m')
+Flight.create(dep_date: Date.new(2020, 10, 20), dep_time: Time.new(2020, 10, 20, 4, 05, 0), origination_id: 2, destination_id: 1, flight_time: '4h52m')
 Flight.create(dep_date: Date.new(2020, 10, 20), dep_time: Time.new(2020, 10, 20, 18, 25, 0), origination_id: 2, destination_id: 1, flight_time: '4h52m')
-Flight.create(dep_date: Date.new(2020, 10, 21), dep_time: Time.new(2020, 10, 21, 8, 43, 0), origination_id: 1, destination_id: 2, flight_time: '4h23m')
-Flight.create(dep_date: Date.new(2020, 10, 21), dep_time: Time.new(2020, 10, 21, 17, 55, 0), origination_id: 2, destination_id: 1, flight_time: '4h46m')
-Flight.create(dep_date: Date.new(2020, 10, 22), dep_time: Time.new(2020, 10, 22, 9, 33, 0), origination_id: 1, destination_id: 2, flight_time: '4h25m')
-Flight.create(dep_date: Date.new(2020, 10, 22), dep_time: Time.new(2020, 10, 22, 18, 56, 0), origination_id: 2, destination_id: 1, flight_time: '4h50m')
+Flight.create(dep_date: Date.new(2020, 10, 21), dep_time: Time.new(2020, 10, 21, 7, 30, 0), origination_id: 1, destination_id: 2, flight_time: '4h23m')
+Flight.create(dep_date: Date.new(2020, 10, 21), dep_time: Time.new(2020, 10, 21, 11, 43, 0), origination_id: 1, destination_id: 2, flight_time: '4h23m')
+Flight.create(dep_date: Date.new(2020, 10, 21), dep_time: Time.new(2020, 10, 21, 8, 55, 0), origination_id: 2, destination_id: 1, flight_time: '4h46m')
+Flight.create(dep_date: Date.new(2020, 10, 21), dep_time: Time.new(2020, 10, 21, 13, 55, 0), origination_id: 2, destination_id: 1, flight_time: '4h46m')
+Flight.create(dep_date: Date.new(2020, 10, 22), dep_time: Time.new(2020, 10, 22, 10, 55, 0), origination_id: 1, destination_id: 2, flight_time: '4h25m')
+Flight.create(dep_date: Date.new(2020, 10, 22), dep_time: Time.new(2020, 10, 22, 14, 33, 0), origination_id: 1, destination_id: 2, flight_time: '4h25m')
+Flight.create(dep_date: Date.new(2020, 10, 22), dep_time: Time.new(2020, 10, 22, 12, 56, 0), origination_id: 2, destination_id: 1, flight_time: '4h50m')
+Flight.create(dep_date: Date.new(2020, 10, 22), dep_time: Time.new(2020, 10, 22, 19, 56, 0), origination_id: 2, destination_id: 1, flight_time: '4h50m')
