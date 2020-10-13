1c0eba5c6bf2bc152669a921db467d0f425f950a
Raymond Oh <raymond.oh@gmail.com>
2020-10-13 12:04:23 -0400

    add more filler info, some add'l cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'flights#index'
   resources :flights
-  resources :bookings
-  get '/bookings_search' => 'bookings#search', as: 'search_bookings'
+  resources :bookings do
+    collection do
+      get 'search'
+    end
+  end
 end
