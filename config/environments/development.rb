1f1dad2122523e6f44550748258c79b8611021a8
roh linux <raymond.oh@gmail.com>
2021-10-21 17:26:14 -0400

    clean up and resolve some minor email (logo) issues.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
